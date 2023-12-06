5b3efac33dff02d75a0dd584a10d2526aed14c22
dahee-lee <dahlee97@gmail.com>
2023-12-06 16:45:45 -0500

    database credentials added

--- app/config/database.config.js
+++ app/config/database.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { DEV_DATABASE_HOST, DEV_DATABASE_USERNAME, DEV_DATABASE_PASSWORD } = process.env
+const { DEV_DATABASE_HOST, DEV_DATABASE_USERNAME, DEV_DATABASE_PASSWORD, PROD_DATABASE_USERNAME, PROD_DATABASE_PASSWORD, PROD_DATABASE_HOST } = process.env
 
 module.exports = {
   development: {
@@ -10,18 +10,11 @@ module.exports = {
     host: DEV_DATABASE_HOST,
     dialect: "postgres"
   },
-  test: {
-    username: DEV_DATABASE_USERNAME,
-    password: DEV_DATABASE_PASSWORD,
-    database: "database_test",
-    host: "127.0.0.1",
-    "dialect": "postgres"
-  },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: PROD_DATABASE_USERNAME,
+    password: PROD_DATABASE_PASSWORD,
+    database: "placesApp_development",
+    host: PROD_DATABASE_HOST,
     dialect: "postgres"
   }
 }
